5b9c82b86034ee43601e31f8f6cbd437ec6c33f7
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-12 12:40:11 +0200

    DbContext set up

---
+++ DataAccess/HabitaroDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class HabitaroDbContext : DbContext
+    {
+        public HabitaroDbContext(DbContextOptions<HabitaroDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Rank> Ranks { get; set; }
+    }
+}
