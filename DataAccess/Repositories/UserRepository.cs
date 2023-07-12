06c7f7a74ac733b9369a5e14693bfa00ec002298
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-12 12:34:23 +0200

    Created UserRepository class

---
+++ DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess.Entities;
+using DataAccess.Repositories.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        private HabitaroDbContext context;
+
+        public UserRepository(HabitaroDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
