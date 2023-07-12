8948d173acb246a62edd820166bf6ab82944fc62
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-12 12:31:38 +0200

    Created IUserRepository interface

---
+++ DataAccess/Repositories/Abstractions/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Abstractions
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAll();
+
+        User GetById(int id);
+
+        void Update(User user);
+
+        void DeleteById(int id);
+
+        void Delete(User user);
+    }
+}
