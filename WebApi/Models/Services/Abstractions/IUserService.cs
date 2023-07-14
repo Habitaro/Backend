88fdc2713468551e4db75e2e87a62a6fa10e08ae
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-14 12:25:46 +0200

    Created IUserService interface

---
+++ WebApi/Models/Services/Abstractions/IUserService.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Models.Services.Abstractions
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAll();
+
+        UserModel? GetById(int id);
+
+        UserModel? GetByEmail(string email);
+
+        void Create(UserModel model);
+
+        void Update(UserModel model);
+    }
+}
