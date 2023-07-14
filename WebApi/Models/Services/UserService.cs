c21663205a622fec29c038c84f98b3c61381d33e
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-14 12:32:26 +0200

    Added UserService class

---
+++ WebApi/Models/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Repositories.Abstractions;
+using WebApi.Models.Services.Abstractions;
+
+namespace WebApi.Models.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository repository;
+
+        public UserService(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(UserModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel? GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UserModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
