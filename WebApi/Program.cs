14e39c63ab1c7609d700f530e07b7427a01ff65a
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-14 17:34:03 +0200

    Added automapper and service manager to services

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,12 @@
+using AutoMapper;
+using WebApi.Models.Services;
+using WebApi.Models.Services.Abstractions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<HabitaroMapProfile>());
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
