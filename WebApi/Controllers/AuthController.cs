66e527c8b8d58d88cf09ffd526594e02d03fc46a
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-17 13:16:01 +0200

    Createrd Auth controller

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Models.Services.Abstractions;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IServiceManager serviceManager;
+        private readonly IConfiguration configuration;
+
+        public AuthController(IServiceManager serviceManager, IConfiguration configuration)
+        {
+            this.serviceManager = serviceManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public IActionResult Register(UserModel model)
+        {
+            serviceManager.UserService.Create(model);
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(UserModel model)
+        {
+            var user = serviceManager.UserService.GetByEmail(model.Email);
+            if (user.Password == model.Password)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
