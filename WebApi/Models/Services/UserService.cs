07047f2ce277c0a0a94b74f771874e784aa2b26b
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-17 20:11:04 +0200

    Minor code refactoring

--- WebApi/Models/Services/UserService.cs
+++ WebApi/Models/Services/UserService.cs
@@ -22,7 +22,6 @@ namespace WebApi.Models.Services
         {
             var model = new UserModel()
             {
-                Id = _repositoryManager.UserRepository.GetAll().Last().Id + 1,
                 Username = dtoModel.Username,
                 Email = dtoModel.Email,
                 Password = dtoModel.Password,
