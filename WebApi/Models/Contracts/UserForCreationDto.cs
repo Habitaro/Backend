470f9afc47dc7e063585f03a10d8a5954ee30522
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-18 15:24:07 +0200

    Added attributes to UserForCreationDto for properties validation

--- WebApi/Models/Contracts/UserForCreationDto.cs
+++ WebApi/Models/Contracts/UserForCreationDto.cs
@@ -1,13 +1,24 @@
-﻿namespace WebApi.Models.Contracts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Contracts
 {
     public class UserForCreationDto
     {
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9]{2,16}$")]
         public string Username { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[/,*\\-+$#@%&])[A-Za-z\\d/,*\\-+$#@%&]{8,}$")]
         public string Password { get; set; }
 
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[/,*\\-+$#@%&])[A-Za-z\\d/,*\\-+$#@%&]{8,}$")]
+        [Compare("Password")]
         public string Password_Confirm { get; set; }
     }
 }
