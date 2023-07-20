eeb226148126d0d725cef2c97f0ddcb4809fecc4
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-07-20 13:27:24 +0200

    Added dto for editing

---
+++ WebApi/Models/Contracts/UserForEditDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Contracts
+{
+    public class UserForEditDto
+    {
+        [StringLength(16, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
+        [RegularExpression("^[a-zA-Z0-9]{2,16}$", ErrorMessage = "Username should contain only letters and digits")]
+        public string? Username { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int? AvatarId { get; set; }
+
+        [StringLength(100, ErrorMessage = "{0} length must be between {2} and {1}.")]
+        public string? Status { get; set; }
+    }
+}
