363fa0b12891f7e33d1cb7411d1c6b2c418b8b28
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-08-09 10:24:13 +0200

    Add test project

---
+++ HabitaroTest/UserRepositoryTest.cs
@@ -0,0 +1,16 @@
+namespace HabitaroTest
+{
+    public class UserRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
