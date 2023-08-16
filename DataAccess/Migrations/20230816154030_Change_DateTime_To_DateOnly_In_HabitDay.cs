db52b49909fdf683e9577d3b5120019bbe378c2f
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-08-16 17:41:30 +0200

    Add DateOnlyConverter, add migration

---
+++ DataAccess/Migrations/20230816154030_Change_DateTime_To_DateOnly_In_HabitDay.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class Change_DateTime_To_DateOnly_In_HabitDay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
