93e05331efe539cb5fe14199c1dce25f877b6625
Kostya_Andrushchenko <kvandrushchenko@gmail.com>
2023-08-30 10:25:24 +0200

    Add hosted servie for seeding data

---
+++ WebApi/Startup/SeedHabitProgressHostedService.cs
@@ -0,0 +1,24 @@
+﻿using WebApi.Models.Services.Abstractions;
+
+namespace WebApi.Startup
+{
+    public class SeedHabitProgressHostedService : IHostedService
+    {
+        public IUnitOfWork Unit { get; set; }
+
+        public SeedHabitProgressHostedService(IUnitOfWork unit)
+        {
+            Unit = unit;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Unit.HabitService.SeedProgress(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
